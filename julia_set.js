bd14a1289d40af1871ec9dc70fe05bd4a785d02d
AuroraChasma <astraignis1@gmail.com>
2024-11-17 22:42:22 +0500

    .

--- julia_set.js
+++ julia_set.js
@@ -1,65 +1,75 @@
 let canvasWidth = 1920;
 let canvasHeight = 1080;
 
-// Julia set parameters
-let cRe = -0.8; // Real part of constant c
-let cIm = 0.156; // Imaginary part of constant c
+//Julia set parameters
+let cRe = -0.8;
+let cIm = 0.156;
 
-function setup() {
+function setup()
+{
   createCanvas(canvasWidth, canvasHeight);
   pixelDensity(1);
 }
 
-function draw() {
+function draw()
+{
   loadPixels();
 
-  let maxIterations = 300;
+  //Ranges of X and Y axis(or Real or Imaginary axis)
+  let xmin = -2.5;
+  let xmax = 2.5;
+  let ymin = -2.5;
+  let ymax = 2.5;
 
-  // Range of the complex plane to display
-  let xmin = -2;
-  let xmax = 2;
-  let ymin = -2;
-  let ymax = 2;
+  let maxIterations = 100;
 
-  for (let x = 0; x < canvasWidth; x++) {
-    for (let y = 0; y < canvasHeight; y++) {
-      // Map pixel coordinates to the complex plane
-      let realComponent = map(x, 0, canvasWidth, xmin, xmax);
-      let imgComponent = map(y, 0, canvasHeight, ymin, ymax);
+  for(let x = 0; x < canvasWidth; x++)
+  {
+    for(let y = 0; y < canvasHeight; y++)
+    {
+      // real and imaginary component of complex number z (a + bi)
+      let realComponent = map(x, 0, canvasWidth, xmin, xmax); // a
+      let imgComponent = map(y, 0, canvasHeight, ymin, ymax); // b
 
-      let iterationNo = 0;
+      //c in z_n+1 = z^2 + c
+      let originalRealComponent = realComponent;
+      let originalImgComponent = imgComponent;
 
-      // Iteratively compute z = z^2 + c
-      while (iterationNo < maxIterations) {
-        // z^2 = (a + bi)^2 = (a^2 - b^2) + (2ab)i
-        let zSquaredRealComponent =
-          realComponent * realComponent - imgComponent * imgComponent;
-        let zSquaredImgComponent = 2 * realComponent * imgComponent;
+      let iterationNo = 1;
+      while(iterationNo <= maxIterations)
+      {
+        //                       z^2 = (a+bi)^2 = (a^2 - b^2) + (2ab)i
+        //Real part of (a+bi)^2   =              a^2            -           b^2
+        let zSquaredRealComponent = realComponent*realComponent - imgComponent*imgComponent;
+        //Img Part of (a+bi)^2    =              2abi
+        let zSquaredImgComponent  = 2 * realComponent * imgComponent;
+        
+        //    z_n+1       =        z^2            +     c 
+        realComponent = zSquaredRealComponent + originalRealComponent;
+        imgComponent  = zSquaredImgComponent +  originalImgComponent;
 
-        // Update z to z^2 + c
-        ///Instead of c being the point on the canva, in julia sets, it is set to a constant
-        realComponent = zSquaredRealComponent + cRe;
-        imgComponent = zSquaredImgComponent + cIm;
-
-        // Check for divergence
-        if (realComponent * realComponent + imgComponent * imgComponent > 4) {
+        //if |z|^2 > 4 or |z| > 2, it is guaranteed that the point is not part of mandelbrot set 
+        if(abs(realComponent*realComponent + imgComponent*imgComponent) > 4)
+        {
           break;
         }
 
         iterationNo++;
       }
 
-      // Map the number of iterations to a color
-      let pixelValue = map(iterationNo, 0, maxIterations, 0, 255);
-      let pixelIndex = (x + y * canvasWidth) * 4;
+      pixelBrightnessValue = map(iterationNo, 0, maxIterations, 0 ,255); // mapping iterationNo to color values(0-255)
+      if(iterationNo === maxIterations) pixelValue = 0; //black if part of mandelbrot set
 
-      pixels[pixelIndex + 0] = pixelValue; // Red
-      pixels[pixelIndex + 1] = pixelValue; // Green
-      pixels[pixelIndex + 2] = pixelValue; // Blue
-      pixels[pixelIndex + 3] = 255; // Alpha
+      //setting pixel colors
+      pix = (x + y * width) * 4;
+      pixels[pix + 0] = pixelBrightnessValue;
+      pixels[pix + 1] = pixelBrightnessValue;
+      pixels[pix + 2] = pixelBrightnessValue;
+      pixels[pix + 3] = 255;
     }
   }
 
   updatePixels();
-  noLoop(); // Stop draw loop for static rendering
+  noLoop();
 }
+
